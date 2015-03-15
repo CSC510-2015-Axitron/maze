7adb5c7ba15ca88f8ed992ce4f2c337aa065c152
Will Morrow <duh102@gmail.com>
2015-03-14 20:54:35 -0400

    got login working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('server.db');
 
 db.serialize(function() {
-    db.run("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, token TEXT, tokenInvalidAt DATE, "
+    db.run("CREATE TABLE IF NOT EXISTS user (id INTEGER NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, "
           +"CONSTRAINT pk_user PRIMARY KEY (id) ON CONFLICT ABORT)");
 
     db.run("CREATE TABLE IF NOT EXISTS mazeCategory (id INTEGER NOT NULL, name TEXT NOT NULL, "
@@ -32,13 +32,16 @@ db.serialize(function() {
         1, "Debug maze 2", false, 2, 2, "{}", 0);
 
     //dummy users
-    db.run("INSERT OR IGNORE INTO user (id, password, email) VALUES (?, ?, ?)", 0, '', 'dummy1@dum.my');
-    db.run("INSERT OR IGNORE INTO user (id, password, email) VALUES (?, ?, ?)", 1, '', 'dummy2@dum.my');
+    db.run("INSERT OR IGNORE INTO user (id, password, email) VALUES (?, ?, ?)", 0,
+        'pec3ZNq6/AnleyBwy3Ft::wYjmgHHjB4zJDYHiH1jnTX7YtQCIq86PgQvfzrsagsnyAk5jKprTsIS4Os3IzGFqhKFqeaH3tkUXSJh3::60::10000',
+        'dummy1@dum.my');//testpassword1
+    db.run("INSERT OR IGNORE INTO user (id, password, email) VALUES (?, ?, ?)", 1,
+        'LJf57nNfbkdcdgWzEqIe::HbrEUuOKIg1TzoWnZBdYBlUy1NqPr+YfL59NLCd7lSksyodThL8xoHN7GdLg6qdQwZ6YtNBcHrRfQ8Os::60::10000',
+        'dummy2@dum.my');//testpassword27
 });
 
 var NodePbkdf2 = require('node-pbkdf2'),
     hasher = new NodePbkdf2({ iterations: 10000, saltLength: 20, derivedKeyLength: 60 });
-var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
 
 function userByAttr(attribute, value, done) {
     doneFunc = done;
@@ -47,56 +50,105 @@ function userByAttr(attribute, value, done) {
             if(row)
                 doneFunc(null, row);
             else
-                doneFunc(new Error('User with email '+email+' does not exist'));
+                doneFunc(new Error('User with identifier '+value+' does not exist'));
         else
             doneFunc(null, null);
     };
     if(attribute === 'email')
-        db.get("SELECT id, password, email, token, tokenInvalidAt FROM user WHERE email = ?", [value], rowFunc(err, row));
+        db.get("SELECT id, password, email FROM user WHERE email = ?", [value], rowFunc);
     else if(attribute === 'id')
-        db.get("SELECT id, password, email, token, tokenInvalidAt FROM user WHERE id = ?", [value], rowFunc(err, row));
+        db.get("SELECT id, password, email FROM user WHERE id = ?", [value], rowFunc);
     else done(null, null);
 }
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
+var uuid = require('node-uuid');
 
-passport.deserializeUser(function(id, done) {
-    userByAttr('id', id, function(err, user) {
-        done(err, user);
+
+//{token:{userid:(id), validUntil:(date)}}
+var tokens = {};
+
+
+//req.body.token is where a login token will both be returned if logging in or
+//stored if being used
+function auth(req, res, next) {
+    if(req.headers.authorization && 
+        tokens[req.headers.authorization] &&
+        tokens[req.headers.authorization].validUntil > new Date()
+    )
+    {
+        var now = new Date();
+        tokens[req.headers.authorization].validUntil = now.setMinutes(now.getMinutes() + 30);
+        next(req, res);
+    }
+    else
+    {
+        res.status(401).json({"response":"unauthorized token"});
+    }
+}
+
+setInterval(function(){
+    var now = new Date();
+    Object.keys(tokens).forEach(function(key){
+        if(tokens[key].validUntil < now) delete tokens[key];
     });
-});
+}, 1000*60*2);
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        findByAttr('email', username, function(err, user) {
-            if(err) return done(err);
-            if(!user) return done(null, false, {message:'Incorrect login'});
-            hasher(password, user.password, function(err, isCorrect) {
-                if(isCorrect)
-                    done(null, user);
+function login(req, res) {
+    if(req.headers.authorization && tokens[req.headers.authorization])
+    {
+        if(tokens[req.headers.authorization].validUntil > new Date())
+        {
+            return  res.status(403).json({"response":"already authorized"});
+        }
+        else
+        {
+            delete tokens[req.headers.authorization];
+        }
+    }
+    if(req.body.email && req.body.password)
+    {
+        userByAttr('email', req.body.email, function(err, user) {
+            if(err) return res.status(500).json({"response":"user error occured"});
+            console.log(user);
+            hasher.checkPassword(req.body.password, user.password, function(err, passCorrect) {
+                if(err) return res.status(500).json({"response":"checkpass error occurred","error":err});
+                if(passCorrect)
+                {
+                    var token = uuid.v4(), dateExpire = new Date();
+                    tokens[token] = {"userid":user.id, "validUntil":(dateExpire.setMinutes(dateExpire.getMinutes() + 30))};
+                    res.status(200).json({"token":token});
+                }
                 else
-                    done(null, false, {message:'Incorrect login'});
+                {
+                    res.status(401).json({"response":"invalid login credentials"});
+                }
             });
         });
     }
-));
+    else
+    {
+        return res.status(401).json({"response":"invalid login credentials"});
+    }
+}
+
+function logout(req, res) {
+    if(req.headers.authorization && tokens[req.headers.authorization])
+    {
+        delete tokens[req.headers.authorization];
+        res.status(200).json({"response":"logged out"});
+    }
+    else
+    {
+        res.status(401).json({"response":"not logged in"});
+    }
+}
 
 var express = require('express');
 var restapi = express();
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 restapi.use(bodyParser.json());
 restapi.set('json spaces', 4);
-restapi.use(session({secret:'amazing mazes'}));
-restapi.use(passport.initialize());
-restapi.use(passport.session());
-
-restapi.all('/', function(req, res) {
-    var sess = req.session
-});
 
 //restapi.all('/play/*', requireAuthentication, loadUser);
 //restapi.all('/user/*', requireAuthentication, loadUser);
@@ -165,37 +217,12 @@ restapi.get("/categories", function(req, res) {
     });
 });
 
-restapi.post('/login', function(req, res, next) {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) { return next(err) }
-        if (!user) {
-            return res.status(401).json({"error":info.message});
-        }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.status(200).json({"response":true});
-        });
-    })(req, res, next);
-});
+restapi.post("/login", login);
 
-restapi.get("/logout", function(req, res) {
-    req.session.destroy(function (err) {
-        if(err) res.status(500).json({"error":err});
-        else res.status(200).json({"response":true});
-    });
-});
+restapi.get("/logout", logout);
 
-restapi.post('/data', function(req, res){
-    db.run("UPDATE counts SET value = value + 1 WHERE key = ?", "counter", function(err, row){
-        if (err){
-            console.err(err);
-            res.status(500);
-        }
-	else {
-	    res.status(202);
-	}	
-	res.end();
-    });
+restapi.all("/", function(req, res) {
+    res.status(404).json({"response":"not found"});
 });
 
 
