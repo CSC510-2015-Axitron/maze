6e0cfe0d817bdd9e82753312b4d1a2cec7134f63
Matthew Neal <meneal@gmail.com>
2015-03-16 11:11:30 -0700

    Working user authorization in apiclient

--- apiClient.js
+++ apiClient.js
@@ -1,18 +1,24 @@
 var rest = require("rest");
 var mime = require('rest/interceptor/mime');
 var pathPrefix = require('rest/interceptor/pathPrefix');
-var basicAuth = require('rest/interceptor/basicAuth')
 
-client = rest.wrap(mime)
-			 .wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com/'})
-			 .wrap(basicAuth, { username: "dummy1@dum.my", password: "testpassword1"});
+var data = JSON.stringify({
+	email: "dummy1@dum.my", 
+	password: "testpassword1"
+});
 
-client({ path: 'login'}).then(function(response) {
-	console.log('response: \n', response);
+client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com/'});
+			//.wrap(mime) 
+			 //.wrap(basicAuth, { email: "dummy1@dum.my", password: "testpassword1"});
+
+var request = { path: "login", method:'POST', 
+				entity: data, headers: {'Content-Type': 'application/json'}};
+				
+client(request).then(function(response) {
+	console.log('response: \n', response.entity);
 	/**
 	for(var i = 0; i < response.entity.length; i++){
 		console.log(response.entity[i].name)
 	}
 	*/
 });
-
