a9a6126774e9dca8f05c8d1a4cccfdfcb61f5ff2
Matthew <meneal@gmail.com>
2015-03-19 15:25:28 -0400

    Finished apiClient added apiuser to use the apiclient, and setup index.html for web demo

--- apiClient.js
+++ apiClient.js
@@ -2,66 +2,102 @@
  * Node based api for axemaze db
  */
 
-
 var rest = require("rest");
 var mime = require('rest/interceptor/mime');
 var pathPrefix = require('rest/interceptor/pathPrefix');
+var apiClient = this;
+
+/**
+ * Function to edit a maze, sorry about the number of params.
+ * Params:
+ * - nm: name of the maze
+ * - catNum: category number
+ * - mapblock: block of information in this form:
+ * {
+ *  width:2,
+ *  height:3,
+ *  start:[0,1],
+ *  end:[1,2],
+ *  board:[[6,5,6],[12,1,8]]
+ * }
+ * - mazeno: number of the maze to be edited
+ * - token: user authorization token
+ * - func: callback returns new maze number if successful
+ *   otherwise returns null.
+ *	
+ */
+apiClient.editMaze = function(nm, catNum, mapBlock, mazeNo, token, func){
+	var data = JSON.stringify({
+		name: nm, 
+		category: catNum,
+		maze: jsobject
+	});
+	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
+	var p = "maze/" + mazeNo;
+	var request = { path: p, method:'POST', entity: data, headers: {'Content-Type': 'application/json', 'authorization': token}};
+	client(request).then(function(response){
+		var ps = JSON.parse(response.entity);
+		if(ps.response === "maze updated"){
+			func(true);
+		}else{
+			func(false);
+		}
+	});
+}
 
 
-//All of these functions are working up until line 34.
-//register("email", "(some password)", function(bool){
-	//console.log(bool);
-//});
+/**
+ * Function to add a new maze
+ * Params:
+ * - nm: name of the maze
+ * - catNum: category number
+ * - mapblock: block of information in this form:
+ * {
+ *  width:2,
+ *  height:3,
+ *  start:[0,1],
+ *  end:[1,2],
+ *  board:[[6,5,6],[12,1,8]]
+ * }
+ * - token: user authorization token
+ * - func: callback returns new maze number if successful
+ *   otherwise returns null.
+ *	
+ */
+apiClient.newMaze = function(nm, catNum, mapBlock, token, func){
+	var data = JSON.stringify({
+		name: nm, 
+		category: catNum,
+		maze: jsobject
+	});
+	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
+	var p = "maze";
+	var request = { path: p, method:'POST', entity: data, headers: {'Content-Type': 'application/json', 'authorization': token}};
+	client(request).then(function(response){
+		var ps = JSON.parse(response.entity);
+		if(ps.mazeno != null){
+				func(ps.mazeno);
+		}else{
+			console.log(response);
+			func(null);
+		}
+	});
+}
 
-//These functions do not work sequentially login plus one of the 
-//other functions works perfectly.
-//login("email", "(some password)", function(token, uid){
-	//console.log(token);
-	//console.log(uid);
-	//authToken = token;
-	//keepAlive(token, function(bool){
-		//console.log(bool);
-	//})
-	//checkInfo(token, uid, function(resp) {
-		//console.log(resp);
-	//});
-	//unameChange("jackson", "pollack", token, uid, function(resp){
-		//console.log(resp);
-	//});
-	//logout(authToken, function(resp){
-		//console.log(resp);
-	//});
-//});
-
-getCategories(function(resp){
-	console.log(resp);
-});
-
-//getTopTen(0, function(resp){
-	//console.log(resp);
-//});
-
-getNumMazes(function(resp){
-	console.log(resp);
-});
-
-getMazesInCategory(1, function(resp){
-	console.log(resp)
-});
-
-getUserTimes(1, "all", function(resp){
-	console.log(resp);
-});
-
-getUserTimes(1, 1, function(resp){
-	console.log(resp);
-});
-
-getMaze(1, function(resp){
-	console.log(resp);
-});
-
-function getMaze(mazeno, func){
+/**
+ * Function to get maze information
+ * Params:
+ * - mazeno: number of the maze to get
+ * - func: call back that returns objects of this form:
+ *{ mazeno: <mazenumber>,
+ * 	displayName: <displayname>,
+ *  userForMaze: <useridnum>,
+ * 	height: <height>,
+ *  width: <width>,
+ *  mazeJSON: '{"width":2,"height":3,"start":[0,1],"end":[1,2],"board":[[6,5,6],[12,1,8]]}',
+ *  category: <categorynum> }
+ */
+apiClient.getMaze = function(mazeno, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'})
 				 .wrap(mime);
 	var p = "maze/" + mazeno;
@@ -71,8 +107,17 @@ function getMaze(mazeno, func){
 	});
 }
 
-//If category is all then return best times for all mazes
-function getUserTimes(uid, category, func){
+/**
+ * Function to get a list of user times.
+ * Params:
+ * - uid: user id number
+ * - category: if category is all it returns all of the categories, 
+ *   otherwise returns whatever category passed
+ * - func: callback returns objects with this format:
+ * 	 { userid: <useridnumber>, category: <categorynumber>, played: [array of times]}
+ *   or error.
+ */
+apiClient.getUserTimes = function(uid, category, func){
 	var p;
 	if(category === "all"){
 		p = "played/" + uid;
@@ -87,17 +132,45 @@ function getUserTimes(uid, category, func){
 	});
 }
 
-function getMazesInCategory(categoryID, func){
+/**
+ * Function to get a list of categories.
+ * Params:
+ * - func: callback returns an array of objects with this format:
+ * { id: <category_id_number>, name: <category_name>}, or error object
+ */
+apiClient.getMazesInCategory = function(categoryID, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'})
 				 .wrap(mime);
-	var p = "mazes/" + categoryID;
+	var p = "mazes/category/" + categoryID;
 	var request = { path: p , method:'GET', headers: {'Content-Type': 'application/json'}};
 	client(request).then(function(response){	
 		func(response.entity);
 	});
 }
 
-function getCategories(func){
+/**
+ * Function to get a list mazes from a particular user
+ * Params:
+ * - userID: userIdNumber
+ * - func: callback returns an array of objects with this format:
+ * { mazeno: <mazenumber>, displayName: <displayname>}, or error msg.
+ */
+apiClient.getMazesByUser = function(userID, func) {
+    client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'}).wrap(mime);
+    var p = "mazes/user/" + userID;
+    var request = { path: p , method:'GET', headers: {'Content-Type': 'application/json'}};
+    client(request).then(function(response){
+        func(response.entity);
+    });
+}
+
+/**
+ * Function to get a list of categories.
+ * Params:
+ * - func: callback returns an array of objects with this format:
+ * { id: <category_id_number>, name: <category_name>}, or error.
+ */
+apiClient.getCategories = function(func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'})
 				 .wrap(mime);
 	var request = { path: 'categories' , method:'GET', headers: {'Content-Type': 'application/json'}};
@@ -106,30 +179,53 @@ function getCategories(func){
 	});
 }
 
-function getTopTen(mazeno, func){
+
+/**
+ * Function to get top ten times on a specific maze. 
+ * Params:
+ * - func: callback returns object with this format:
+ * { response: response info }, or error
+ *
+ */
+apiClient.getTopTen = function(mazeno, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'})
 				 .wrap(mime);
-	var p = "topten/" + mazeno;
+	var p = "top10/" + mazeno;
 	var request = { path: p , method:'GET', headers: {'Content-Type': 'application/json'}};
 	client(request).then(function(response){	
 		func(response.entity);
 	});
 }
 
-function getNumMazes(func){
+
+/**
+ * Function to get total number of mazes in the db
+ * Params:
+ * - func: callback that returns the number of mazes 
+ * as its only parameter. 
+ */
+apiClient.getNumMazes = function(func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'})
 				 .wrap(mime);
 	var request = { path: 'mazes' , method:'GET', headers: {'Content-Type': 'application/json'}};
 	client(request).then(function(response){	
-		func(response.entity);
+		//var ps = JSON.parse(response.entity);
+		func(response.entity.mazes);
 	});
 }
 
-function keepAlive(token, func){
+/**
+ * Function to keep user account alive
+ * Params:
+ * - token: token of the logged in user
+ * - func: call back that returns true if successful
+ *   false otherwise.
+ *
+ */
+apiClient.keepAlive = function(token, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
 	var request = { path: 'keepalive' , method:'GET', headers: {'Content-Type': 'application/json', 'authorization': token}};
 	client(request).then(function(response){
-		//console.log("response entity " + response.entity);
 		var ps = JSON.parse(response.entity);
 		var resp = ps.response;
 		func(resp);
@@ -137,18 +233,38 @@ function keepAlive(token, func){
 }
 
 
-function checkInfo(token, uid, func){
+
+/**
+ * Function to get user information
+ * Params:
+ * - token: token of the logged in user
+ * - uid: user id number
+ * - func: call back that returns userid and email 
+ * in that order as parameters.
+ *
+ */
+apiClient.checkInfo = function(token, uid, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
 	var p = "user/" + uid;
 	var request = { path: p, method:'GET', headers: {'Content-Type': 'application/json', 'authorization': token}};
 	client(request).then(function(response){
-		console.log("response entity "+ response.entity);
-		func(response.entity);
+		var ps = JSON.parse(response.entity);
+		func(ps.userid, ps.email);
 	});
 }
 
-
-function unameChange(em, pw, token, uid, func){
+/**
+ * Function to change usernames.
+ * Params:
+ * - em: Email adress of the user
+ * - pw: Password of the user
+ * - token: token of the logged in user
+ * - uid: user id number
+ * - func: call back that returns true if the name change is successful,
+ * returns false if some failure.
+ *
+ */
+apiClient.unameChange = function(em, pw, token, uid, func){
 	var data = JSON.stringify({
 		email: em,
 		password: pw
@@ -157,12 +273,28 @@ function unameChange(em, pw, token, uid, func){
 	var p = "user/" + uid;
 	var request = { path: p, method:'POST', entity: data, headers: {'Content-Type': 'application/json', 'authorization': token}};
 	client(request).then(function(response){
-		console.log("response entity "+ response.entity);
-		func(response.entity);
+		var ps = JSON.parse(response.entity);
+		var val = ps.response;
+		if(val === "user updated"){
+			func(true);
+		}else{
+			func(false);
+		}
 	});
 }
 
-function register(em, pw, func) {
+
+
+/**
+ * Register function for registering new users.
+ * Params:
+ * - em: Email adress of the user
+ * - pw: Password of the user
+ * - func: call back that returns true if the user is already registered, or
+ *   registration is successful, returns false if some failure.
+ *
+ */
+apiClient.register = function(em, pw, func) {
 	var data = JSON.stringify({
 		email: em,
 		password: pw
@@ -170,9 +302,7 @@ function register(em, pw, func) {
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
 	var request = { path: "register", method:'POST', 
 				entity: data, headers: {'Content-Type': 'application/json'}};
-
 	client(request).then(function(response) {
-		//console.log("response entity " + response.entity);
 		if(response.entity != null){
 			var ps = JSON.parse(response.entity);
 			var resp = ps.response;
@@ -186,10 +316,15 @@ function register(em, pw, func) {
 
 
 /**
- * Simple login function, returns a boolean in a callback function
- * true if login successful, false if failed.
+ * Simple login function.
+ * Params:
+ * - em: Email adress of the user
+ * - pw: Password of the user
+ * - func: call back that returns the token and userid of the authorized
+ * - user, or returns null if login fails.
+ *
  */
-function login(em, pw, func) {
+apiClient.login = function(em, pw, func) {
 	var data = JSON.stringify({
 		email: em,
 		password: pw
@@ -197,14 +332,12 @@ function login(em, pw, func) {
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
 	var request = { path: "login", method:'POST', 
 				entity: data, headers: {'Content-Type': 'application/json'}};
-
 	client(request).then(function(response) {
 		console.log("response entity " + response.entity);
 		if(response.entity != null){
 			var ps = JSON.parse(response.entity);
 			var tk = ps.token;
 			var uid = ps.userid;
-
 			if(tk.length == 36)
 				func(tk,uid);
 		}else{
@@ -213,12 +346,28 @@ function login(em, pw, func) {
 	});
 };
 
-function logout(token, func){
+/**
+ * Logout function does what you expect.  
+ *
+ * Params:  
+ * - token: The auth token of the user that wants to logout
+ * - func: A callback function to return the response.  Returns
+ * true on a successful logout, false otherwise.
+ *
+ */
+apiClient.logout = function(token, func){
 	client = rest.wrap(pathPrefix, { prefix: 'http://axemaze-db.herokuapp.com'});
 	var request = { path: "logout", method:'GET', headers: {'Content-Type': 'application/json', 'authorization': token}};
 	client(request).then(function(response){
-		console.log("response entity "+ response.entity);
-		func(response.entity);
+		var ps = JSON.parse(response.entity);
+		var val = ps.response;
+		//console.log("resp.entity.resp  is " + val);
+		if(val === "logged out"){
+			func(true);
+		}else{
+			func(false);
+		}
 	});
 };
 
+module.exports = apiClient;
---
+++ apiuser.js
@@ -0,0 +1,91 @@
+/**
+ * Api user is just a simple set of example user commands for the apiClient
+ */
+
+var apiClient = require('./apiClient.js');
+
+//====================================================================
+// Example usage here
+//====================================================================
+
+//All of these functions are working up until line 34.
+//apiClient.register("email", "(some password)", function(bool){
+	//console.log(bool);
+//});
+
+//These functions do not work sequentially login plus one of the 
+//other functions works perfectly.
+//apiClient.login("master", "commander", function(token, uid){
+	//console.log(token);
+	//console.log(uid);
+	//authToken = token;
+
+	//apiClient.newMaze("Pit", 1, jsobject, authToken, function(resp){
+		//console.log(resp);
+	//})
+
+	//apiClient.editMaze("Pit", 1, jsobject, 18, authToken, function(resp){
+		//console.log(resp);
+	//})
+
+	//apiClient.keepAlive(token, function(bool){
+		//console.log(bool);
+	//})
+	//apiClient.checkInfo(token, uid, function(uid, email) {
+		//console.log(uid, email);
+	//});
+	//apiClient.unameChange("master2", "commander2", token, uid, function(resp){
+		//console.log(resp);
+	//});
+	//apiClient.logout(authToken, function(resp){
+		//console.log(resp);
+	//});
+//});
+
+
+apiClient.getCategories(function(resp){
+	console.log("get categories: " + resp);
+});
+
+apiClient.getTopTen(0, function(resp){
+	console.log("get top ten " + resp);
+});
+
+apiClient.getNumMazes(function(resp){
+	console.log("get numMazes " + resp);
+});
+
+apiClient.getMazesInCategory(1, function(resp){
+	console.log("get mazes in cat " + resp)
+});
+
+apiClient.getUserTimes(1, "all", function(resp){
+	console.log("get user times " + resp);
+});
+
+apiClient.getUserTimes(1, 1, function(resp){
+	console.log("get user times " + resp);
+});
+
+apiClient.getUserTimes(1, 100, function(resp){
+	console.log("get user times individual " + resp);
+});
+
+apiClient.getMaze(13, function(resp){
+	console.log("get maze " + resp);
+});
+
+
+apiClient.getMazesByUser(32, function(resp){
+	console.log("get mazes by user " + resp);
+});
+
+
+
+//var jsobject = {
+  //width:2,
+  //height:3,
+  //start:[0,1],
+  //end:[1,2],
+  //board:[[6,5,6],[12,1,8]]
+//}
