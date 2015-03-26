778b21be97c4df3a62f33c2b4add5f10d05c63ff
Will Morrow <duh102@gmail.com>
2015-03-26 12:07:58 -0400

    well on the way to recreating designer

--- designer/designer.js
+++ designer/designer.js
@@ -10,6 +10,14 @@ $(function() {
 	codeArea = $('#code_textarea'),
 	cellWidth = 20,//probably should find a better way to do this
 	cellHeight = 20,//make sure this and the css match exactly
+	verticalPathCells = $(),
+	horizontalPathCells = $(),
+	centerPathCells = $(),
+	startDiv,
+	endDiv,
+	lastSelectedObject,
+	lastSelectedTime,
+	toolOnStart = true,
 
 	//make a table row with a number of cells
 	tRow = function(numCells) {
@@ -19,6 +27,31 @@ $(function() {
 	//make a table with a number of rows and columns
 	tTable = function(numRows, numColumns) {
 		return Array.apply(null,Array(numRows+1)).join(tRow(numColumns)+"\n");
+	},
+
+	wallClick = function(x,y,direction) {
+
+	},
+	cellClick = function(x,y,direction) {
+		var now = new Date();
+		if($( this ).is(lastSelectedObject) && (now - lastSelectedTime) < 300)//0.3 s
+		{
+			if(toolOnStart && startDiv) startDiv.removeClass('start').text('');
+			if(!toolOnStart && endDiv) endDiv.removeClass('end').text('');
+
+			if(toolOnStart)
+			{
+				$( this ).text('s').addClass('start');
+				startDiv = $( this );
+			}
+			else
+			{
+				$( this ).text('e').addClass('end');
+				endDiv = $( this );
+			}
+		}
+		lastSelectedObject = $( this );
+		lastSelectedTime = now;
 	},
 
 	//Gets the size from the input elements, then initializes the designer using them.
@@ -41,18 +74,38 @@ $(function() {
 			maze.board.push(temp);
 		}
 
+		horizontalPathCells.off('click');
+		verticalPathCells.off('click');
+		centerPathCells.off('click');
+
 		mtbl.css({width:cellWidth*(maze.width*2-1), height:cellHeight*(maze.height*2-1)});
 		mtbl.html(tTable(maze.height*2-1, maze.width*2-1));
-		//problem: css is applicable ALL THE TIME
-		//maybe don't use nth child
-		for(var x = 1; x < (maze.width*2-1)+1; x++)
+
+		horizontalPathCells = $();
+		verticalPathCells = $();
+		centerPathCells = $();
+
+
+		for(var x = 1; x < maze.width+1; x++)
 		{
-			mtbl.find(':nth-child('+((maze.width*2-1)*2)+'n'+x+')').css({background:'#cac'});
+			if(x < maze.width)
+			{
+				horizontalPathCells = mtbl.find(':nth-child('+(2*(maze.width*2-1))+'n-'+(2*(maze.width*2-1)-(2*x))+')')
+					.add(horizontalPathCells).addClass('mazeui').addClass('mazeUnSelectedWall');
+			}
+			verticalPathCells = mtbl.find(':nth-child('+(2*(maze.width*2-1))+'n-'+((maze.width*2-1)-(2*x-1))+')')
+				.add(verticalPathCells).addClass('mazeui').addClass('mazeUnSelectedWall');
+			centerPathCells = mtbl.find(':nth-child('+(2*(maze.width*2-1))+'n-'+(2*(maze.width*2-1)-(2*x-1))+')')
+				.add(centerPathCells).addClass('mazeui').addClass('mazeCell');
 		}
-		for(var x = 1; x < (maze.width*2-1)+1; x++)
-		{
-			mtbl.find(':nth-child('+(maze.width*2-1)+'n-'+((maze.width*2-1)-(2*x-1))+')').css({background:'#acc'});
-		}
+
+		//horizontalPathCells.text("--");
+		//verticalPathCells.text("|");
+		//centerPathCells.text("c");
+
+		centerPathCells.on('click', cellClick);
+		horizontalPathCells.on('click', wallClick);
+		verticalPathCells.on('click', wallClick);
 
 		updateMazeCode();
 	},
@@ -63,6 +116,10 @@ $(function() {
 	};
 
 	$('#updateSize').click(setSize);
+
+	$('#t_start').click(function(){toolOnStart = true;});
+	$('#t_end').click(function(){toolOnStart = false});
+	toolOnStart = $('#t_start').is(':checked');
 
 	setSize();
 });
