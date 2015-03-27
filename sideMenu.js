9c739c0b49a94aa99316f2099255b5a8b271224f
Matthew Neal <meneal@gmail.com>
2015-03-27 12:47:41 -0400

    Got side menu working and initial calls from api working

---
+++ sideMenu.js
@@ -0,0 +1,67 @@
+
+var categories =[
+    {
+        "id": 1,
+        "name": "Small Mazes (5-10)"
+    },
+    {
+        "id": 101,
+        "name": "Medium Mazes (10-20)"
+    },
+    {
+        "id": 201,
+        "name": "Large Mazes (20-30)"
+    },
+    {
+        "id": 301,
+        "name": "Huge Mazes (30+)"
+    }
+];
+
+
+var buildCats = function (){
+    var sub
+    for(var i = 0; i < categories.length; i++){  
+        apiClient.getMazesInCategory(categories[i].id, function(resp){
+            var count = 1;
+            for(var j = 0; j < resp.mazes.length; j++){
+                var x = $('<li id=' + resp.mazes[j].mazeno + '><a href="#">' + resp.mazes[j].displayName + '</a></li>');
+                sub = $('#sub' + (count));
+                sub.append(x);
+                console.log("Count is " + count);
+            }
+            //I just did this weird count thing because something bizzare was happening to i
+            //I'm assuming it has something to do with the async part of this.
+            count++;
+        });
+    };
+};
+
+ $(document).ready(function() {
+      $('#left-menu').sidr({
+          name: 'sidr', 
+          speed: 200, 
+          side: 'left',
+          source: null, 
+          renaming: true, 
+          body: 'body'
+
+         });
+    });
+
+$(document).ready(function() {
+    buildCats();
+    $('.sub-menu-sidr').hide();
+
+    $("#sidr li:has(ul)").click(function(){
+        $("ul",this).toggle('fast');
+    });
+
+    $("li").click(function(){
+       var curId = $(this).attr('id');
+       if(typeof curId !== 'undefined'){
+            console.log('id is ' + curId);
+       }
+    });   
+});
+
